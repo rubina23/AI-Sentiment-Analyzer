f33efa779b80bbfc2e749ce02e24fc90f3bd926b
Rubina Begum <121657663+rubina23@users.noreply.github.com>
2026-06-07 12:43:42 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from youtube_comment_downloader import *
 import itertools
 
 # ওয়েবসাইটের মূল সেটিং
-st.set_page_config(page_title="AI Sentiment Analyzer", page_icon="🧠", layout="centered")
+st.set_page_config(page_title="AI Sentiment Analyzer", page_icon="🧠", layout="wide")
 
 st.title("🧠 AI Sentiment Analyzer")
 st.write("Analyze Sentiments, Detect Emotions, Generate Auto-Replies, and Scrape Live YouTube Comments!")
